ccf56452568043c7596fb38f021231ff3b04bc57
KungFuKevin <kevin.voet@student.nhlstenden.com>
2024-01-18 13:15:37 +0100

    Adding Routes, now checks whether a route is already in use and won't create a new route.

--- FrisianPortsREST_API/Repositories/CargoTransportRouteRepository.cs
+++ FrisianPortsREST_API/Repositories/CargoTransportRouteRepository.cs
@@ -10,15 +10,23 @@ namespace FrisianPortsREST_API.Repositories
             using (var connection = DBConnection.GetConnection())
             {
                 connection.Open();
-                const string addQuery = @$"INSERT INTO ROUTE
-                                            (DEPARTURE_PORT_ID, ARRIVAL_PORT_ID)
-                                            VALUES
-                                            (@DeparturePortId,@ArrivalPortId);
 
-                                            INSERT INTO cargotransport
-                                            (FREQUENCY, DATE_STARTED, ADDED_BY_ID, ROUTE_ID)
-                                            VALUES
-                                            (@Frequency,@DateStarted,@AddedById,(SELECT LAST_INSERT_ID()));";
+                int alreadyExist = checkRouteExists(cargoTransportRoute);
+                string addQuery = $@"INSERT INTO ROUTE (DEPARTURE_PORT_ID, ARRIVAL_PORT_ID) 
+                                    VALUES (@DeparturePortId,@ArrivalPortId);
+
+                                    INSERT INTO cargotransport
+                                    (FREQUENCY, DATE_STARTED, ADDED_BY_ID, ROUTE_ID)
+                                    VALUES
+                                    (@Frequency,@DateStarted,@AddedById,(SELECT LAST_INSERT_ID()));";
+
+                if (alreadyExist != 0) 
+                {
+                    addQuery = $@"INSERT INTO cargotransport
+                    (FREQUENCY, DATE_STARTED, ADDED_BY_ID, ROUTE_ID)
+                    VALUES
+                    (@Frequency,@DateStarted,@AddedById,@RouteId);";
+                }
 
                 int idOfCreated = await connection.ExecuteScalarAsync<int>(addQuery,
                    new
@@ -28,10 +36,34 @@ namespace FrisianPortsREST_API.Repositories
                        Frequency = cargoTransportRoute.Frequency,
                        DateStarted = cargoTransportRoute.Date_Started,
                        AddedById = cargoTransportRoute.Added_By_Id,
+                       RouteId = alreadyExist
                    });
                 connection.Close();
                 return idOfCreated;
             }        
+        }
+
+        public int checkRouteExists(CargoTransportRouteDTO cargoTransportRoute) 
+        {
+            using (var connection = DBConnection.GetConnection())
+            {
+                connection.Open();
+
+                const string addQuery = @$"SELECT ROUTE_ID FROM ROUTE";
+
+                QueryBuilder queryBuilder = new QueryBuilder(addQuery);
+                queryBuilder.AddFilter("DEPARTURE_PORT_ID = @DeparturePortId");
+                queryBuilder.AddFilter("ARRIVAL_PORT_ID = @ArrivalPortId");
+
+                int idOfCreated = connection.ExecuteScalar<int>(queryBuilder.Build(),
+                   new
+                   {
+                       DeparturePortId = cargoTransportRoute.Departure_Port_Id,
+                       ArrivalPortId = cargoTransportRoute.Arrival_Port_Id,
+                   });
+                connection.Close();
+                return idOfCreated;
+            }
         }
     }
 }
